7c3c6e3d2689f16df03765a402306ac17cedde8c
snsr777 <48439115+snsr777@users.noreply.github.com>
2019-11-03 18:33:19 +0530

    Update SecondProgram.java

--- SecondProgram.java
+++ SecondProgram.java
@@ -1,7 +1,6 @@
 public class SecondProgram{
-	public static void main(String[] args){
-		System.out.println("Welcome to Second world");
-	}
+	publxic static void main(String[] args){
+		System.out.println("Welcome to Second world War")
 }
 
 
